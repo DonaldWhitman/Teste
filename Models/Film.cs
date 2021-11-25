ca12c3d0feebfa2eb030b2a3459499f76f126b98
DonaldWhitman <78176259+DonaldWhitman@users.noreply.github.com>
2021-11-25 18:15:39 -0300

    Add files via upload

---
+++ Models/Film.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraAPI.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Título { get; set; }
+        public string Diretor { get; set; }
+        public string Sinopse { get; set; }
+        public DateTime Data_Lancamento { get; set; }
+    }
+}
---
+++ Repositories/IFilmRepository.cs
@@ -0,0 +1,17 @@
+﻿using LocadoraAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocadoraAPI.Repositories
+{
+    public interface IFilmRepository
+    {
+        Task<IEnumerable<Film>> Get();
+        Task<Film> Get(int id);
+        Task<Film> Create(Film film);
+        Task Update(Film film);
+        Task Delete(int id);
+    }
+}
