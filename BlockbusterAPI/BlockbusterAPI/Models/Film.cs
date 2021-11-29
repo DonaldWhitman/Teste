1909ef729247b0bd8b854f33e5274b9e38483e23
DonaldWhitman <78176259+DonaldWhitman@users.noreply.github.com>
2021-11-28 22:16:16 -0300

    Add files via upload

---
+++ BlockbusterAPI/BlockbusterAPI/Models/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockbusterAPI.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Synopsis { get; set; }
+        public string Release { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ BlockbusterAPI/BlockbusterAPI/Models/FilmContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockbusterAPI.Models
+{
+    public class FilmContext : DbContext
+    {
+        public FilmContext(DbContextOptions<FilmContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Film> Films { get; set; }
+    }
+}
